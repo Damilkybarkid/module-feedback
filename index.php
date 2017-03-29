00cf819cba4232902cc558835079e891113c90c4
Bell <40200288@soc-web-liv-06.napier.ac.uk>
2017-03-29 14:43:14 +0100

    Finished first video tutorial. Got name from database.

--- index.php
+++ index.php
@@ -1,2 +1,27 @@
 <?php
-print "Module feedback";
+print "<h1>Module feedback</h1>";
+if (!array_key_exists('u',$_REQUEST))
+{
+	print "Who are you <form><input name=u value='50200036' type=submit></form>";
+	exit();
+}
+$con = new mysqli('localhost','scott','tiger','gisq');
+if ($con ->connect_error)
+{
+	die('Connection failure');
+}
+$sql = "select SPR_FNM1,SPR_SURN from INS_SPR where SPR_CODE=?";
+$stmt = $con->prepare($sql)
+	or die($con->error);
+$stmt->bind_param('s',$_REQUEST['u'])
+	or die('Bind error');
+$stmt->execute()
+	or die('Execute error');
+$cur = $stmt->get_result();
+if (!($row = $cur->fetch_array()))
+{
+	echo 'Invalid matric number';
+	exit();
+}
+print "Welcome student: ".$row[0].' '.$row[1];
+
