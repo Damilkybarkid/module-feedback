c2172a813c5193a6f836d9e9e1ce6710018fa67f
Bell <40200288@soc-web-liv-06.napier.ac.uk>
2017-04-04 13:16:01 +0100

    Completed all videos apart from final one. Managed to implement all 19 questions for each module. Implemented own database and radio buttons for answer selection. Need to change question numbers.

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 print "<link rel=stylesheet href=mf.css>";
-print "<h1>Module feedback</h1>";
+print "<h1>Module Feedback</h1>";
 if (!array_key_exists('u',$_REQUEST))
 {
-	print "Who are you <form><input name=u value='50200036' type=submit></form>";
+	print "Who are you <form><input name=u value='50200036'/><input type=submit value='Submit'/></form>";
 	exit();
 }
-$con = new mysqli('localhost','scott','tiger','gisq');
+$con = new mysqli('localhost','40200288','FRJPhjN/','40200288');
 if ($con ->connect_error)
 {
 	die('Connection failure');
@@ -26,8 +26,19 @@ if (!($row = $cur->fetch_array()))
 	echo 'Invalid matric number';
 	exit();
 }
-print "Welcome student: ".$row[0].' '.$row[1];
 $stmt->close();
+
+# Get questions
+$sql = "select QUE_TEXT from INS_QUE";
+$stmt = $con->prepare($sql)
+	or die($con->error);
+$stmt->execute()
+	or die('Execute error');
+$cur = $stmt->get_result();
+$q1 = $cur->fetch_all();
+$stmt->close();
+
+print "Welcome student: ".$row[0].' '.$row[1];
 
 # Get a list of modules
 $sql = "select CAM_SMO.MOD_CODE, MOD_NAME, INS_MOD.PRS_CODE, PRS_FNM1, PRS_SURN
@@ -41,10 +52,26 @@ $stmt->bind_param('s',$_REQUEST['u'])
 $stmt->execute()
 	or die('Execute error');
 $cur = $stmt->get_result();
+print "<form action=store_fb.php>";
 while ($row = $cur->fetch_row())
 {
 	print "<div class=module>";
 	print "<h2>Please answer questions about $row[0]</h2>";
-	print "<p>What did you think of it?</p>";
+	for($i=0;$i<19;$i++)
+	{
+		$question=str_replace(array('[', ']'), '', htmlspecialchars(json_encode($q1[$i]), ENT_NOQUOTES));
+		$question=str_replace(array('"', '"'), '', htmlspecialchars($question, ENT_NOQUOTES));
+		print "<p>$question</p>";
+		print "
+			<input type='radio' name='$row[0]_Q1d1' value='5'> DA
+			<input type='radio' name='$row[0]_Q1d1' value='4'> MA
+			<input type='radio' name='$row[0]_Q1d1' value='3'> N
+			<input type='radio' name='$row[0]_Q1d1' value='2'> MD
+			<input type='radio' name='$row[0]_Q1d1' value='1'> DD
+	      	      ";
+	}	
 	print "</div>";
 }
+print "<input type=hidden name=u value=$_REQUEST[u]>";
+print "<input type=submit>";
+print "</form>";
---
+++ store_fb.php
@@ -0,0 +1,42 @@
+<?php
+if (!array_key_exists('u',$_REQUEST))
+{
+	die("Who are you");
+}
+$con = new mysqli('localhost','40200288','FRJPhjN/','40200288');
+if ($con ->connect_error)
+{
+	die('Connection failure');
+}
+# Delete all records before inserting new ones - possibly a bad decision TODO stop being bad
+$sql = "delete from INS_RES where SPR_CODE=? AND AYR_CODE='2016/7' AND PSL_CODE='TR1'";
+$stmt = $con->prepare($sql)
+	or die($con->error);
+$stmt->bind_param('s',$_REQUEST['u'])
+	or die('Bind error');
+$stmt->execute()
+	or die('Failed to delete'.$con->error);
+
+$sql = "insert into INS_RES VALUES (?,?,'2016/7','TR1',?,?)";
+$stmt = $con->prepare($sql)
+        or die($con->error);
+$stmt->bind_param('sssi',$_REQUEST['u'],$mc,$qn,$res)
+	or die('Bind error');
+foreach($_REQUEST as $k => $v)
+{
+	if ($k == 'u')
+	{
+		continue;
+	}
+	$k = preg_replace('/d/','.',$k);
+	print "<li>".$k;
+	print "<li>".$v;
+	$mc_qn = preg_split('/_Q/',$k);
+	$mc = $mc_qn[0];
+	$qn = $mc_qn[1];
+	$res = $v;
+	$stmt->execute()
+		or die('Execute error: '.$con->error);
+	print "<li>done and insert ,$_REQUEST[u],$mc,$qn,$res";
+}
+print json_encode($_REQUEST);
